66981348041d9e98966b86bd25fa5d435ddebe02
Fabio <fabio.cebral.7e5@itb.cat>
2024-01-23 18:45:46 +0100

    Primer comit

--- src/stores/AuthUserStore.js
+++ src/stores/AuthUserStore.js
@@ -2,7 +2,7 @@ import { defineStore } from "pinia"
 export const useAuthUserStore = defineStore("AuthUserStore", {
     state: () => {
         return {
-            username: "Carlos",
+            username: "DonFabio",
         }
     }
 })
